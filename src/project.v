6f600cbeb35b5da18f6f025f0413ed0623c8c9d4
Andrew Dona-Couch <hi@andrewcou.ch>
2024-11-08 15:47:25 -0500

    import tt06-cora16

--- src/project.v
+++ src/project.v
@@ -1,27 +1,51 @@
 /*
- * Copyright (c) 2024 Your Name
+ * Copyright (c) 2024 Andrew Dona-Couch
  * SPDX-License-Identifier: Apache-2.0
  */
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_couchand_cora16 (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // always 1 when the design is powered, so you can ignore it
+    input  wire       ena,      // will go high when the design is enabled
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
-  assign uio_out = 0;
-  assign uio_oe  = 0;
+  // Allow external SPI RAM programming on reset
+  assign uio_oe  = rst_n ? 8'b11100111 : 8'b11100000;
 
-  // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  assign uio_out[4:3] = 0;
+
+  wire spi_miso, spi_select, spi_clk, spi_mosi;
+  assign spi_miso = uio_in[3];
+  assign uio_out[1] = spi_select;
+  assign uio_out[2] = spi_clk;
+  assign uio_out[0] = spi_mosi;
+
+  wire step, busy, halt, trap;
+  assign step = uio_in[4];
+  assign uio_out[5] = busy;
+  assign uio_out[6] = halt;
+  assign uio_out[7] = trap;
+
+  cpu cpu_instance(
+    .clk(clk),
+    .rst_n(rst_n),
+    .spi_mosi(spi_mosi),
+    .spi_select(spi_select),
+    .spi_clk(spi_clk),
+    .spi_miso(spi_miso),
+    .step(step),
+    .busy(busy),
+    .halt(halt),
+    .trap(trap),
+    .data_in(ui_in),
+    .data_out(uo_out)
+  );
 
 endmodule
